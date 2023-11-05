329db01c2f92e754952e1577ec0392452a476bd1
JosueSay <106031855+JosueSay@users.noreply.github.com>
2023-11-04 18:52:40 -0600

    posiciones nuevas electrones

--- Parte 3/Simulacion_Cilindro.py
+++ Parte 3/Simulacion_Cilindro.py
@@ -3,6 +3,7 @@ from Posiciones import damePosiciones  # Obtener posiciones y & z para la creaci
 
 # Variables globales para gestionar el canvas y la escena
 espacio = None
+electron_sim = None  # Variable para controlar el bucle de la simulación
 
 def crearElectrones(cant_electrones, centro_cilindro, radio_cilindro, radio_electron, velocidad_electron):
     electrones = []
@@ -25,7 +26,7 @@ def cerrarSimulacionCilindro():
     if espacio:
         espacio.delete()
         espacio = None
-        
+
 def cerrarSimulacionActual():
     """Cierra el canvas actual si existe."""
     global espacio
@@ -39,35 +40,32 @@ def iniciarSimulacionCilindro(diametro_c, longitud_c, densidad_e, velocidad_e):
 def simulacionCilindro(diametro_c, longitud_c, densidad_e, velocidad_e):
     longitud = int(longitud_c)
     radio_cilindro = int(diametro_c/2)
-    cant_electrones = 50
     radio_electron = int((radio_cilindro / 20) * 0.1) if radio_cilindro >= 20 else 0.1
     velocidad_electron = 0.1
-
-    # Función para actualizar la velocidad de los electrones
-    def cambiarVelocidad(new_vel):
-        for electron in electrones:
-            electron.direccion = vector(new_vel, 0, 0)
-            electron.velocidad = new_vel
-
-    # Configuración de la escena
-    espacio = canvas(title='Simulación', width=1920, height=1080)
-
-    # Calcular la mitad de la longitud para posicionar en el centro
     centro_cilindro = longitud / 2
+    cant_electrones = 100
 
-    # Crear cilindro azul horizontal vacío en el centro de la escena
-    cable = cylinder(pos=vector(-centro_cilindro, 0, 0), axis=vector(longitud, 0, 0), radius=radio_cilindro, color=color.blue, opacity=0.5)
-
-    # Llama a la función crearElectrones para generar los electrones
-    electrones = crearElectrones(cant_electrones, centro_cilindro, radio_cilindro, radio_electron, velocidad_electron)
-
-    # Movimiento de los electrones
     while True:
-        rate(100)  # Controla la velocidad de la animación
-        for electron in electrones:
-            electron.pos += electron.direccion
-            # Si el electrón sale del cable, reinicia su posición al inicio del cable
-            if electron.pos.x > centro_cilindro:
-                electron.pos.x = -centro_cilindro
-            
+        # Configuración de la escena
+        espacio = canvas(title='Simulación', width=1920, height=1080)
+        # Crear cilindro azul horizontal vacío en el centro de la escena
+        cable = cylinder(pos=vector(-centro_cilindro, 0, 0), axis=vector(longitud, 0, 0), radius=radio_cilindro, color=color.blue, opacity=0.5)
+
+        # Llama a la función crearElectrones para generar los electrones
+        electrones = crearElectrones(cant_electrones, centro_cilindro, radio_cilindro, radio_electron, velocidad_electron)
+
+        while True:
+            rate(100)  # Controla la velocidad de la animación
+            for electron in electrones:
+                electron.pos += electron.direccion
+                # Si el electrón sale del cable, se destruye y se genera uno nuevo
+                if electron.pos.x > centro_cilindro:
+                    electron.visible = False  # Ocultar el electrón
+                    electrones.remove(electron)  # Eliminar el electrón de la lista
+                    # Crear un nuevo electrón
+                    pY, pZ = damePosiciones(-centro_cilindro, radio_cilindro, radio_electron)
+                    nuevo_electron = sphere(pos=vector(-centro_cilindro, pY, pZ), radius=radio_electron, color=color.red)
+                    nuevo_electron.direccion = vector(velocidad_electron, 0, 0)  # Establecer dirección inicial
+                    nuevo_electron.velocidad_electron = velocidad_electron  # Establecer velocidad
+                    electrones.append(nuevo_electron)  # Agregar nuevo electrón a la lista
 iniciarSimulacionCilindro(20, 10, 5, 2)
